296fe2679d0f8b4a76a74864cda60a52c9d1eb45
patchino76 <patchino76@gmail.com>
2017-10-30 19:38:03 +0200

    ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> addOne = n => n + 1;
+
+            Func<int, int, int> addNums = (x, y) => x + y;
+            Func<int, bool> isZero = n => n == 0;
+            Console.WriteLine(addOne(5)); // 6
+            Console.WriteLine(isZero(addNums(-5, 5))); // True
+            int[] a = { 0, 1, 2, 0, 4, 0, 1 };
+            Console.WriteLine(a.Count(isZero)); // 3
+
+        }
+    }
+
+    public class Rectangle
+    {
+        public int Length { get; set; }
+        public int Height { get; set; }
+
+        public void Grow(int length, int height)
+        {
+            Length += length;
+            Height += height;
+        }
+    }
+
+    public class ImmutableRectangle
+    {
+        int Length { get; }
+        int Height { get; }
+        public ImmutableRectangle(int length, int height)
+        {
+            Length = length;
+            Height = height;
+        }
+        public ImmutableRectangle Grow(int length, int height) =>    new ImmutableRectangle(Length + length, Height + height);
+    }
+
+
+}
